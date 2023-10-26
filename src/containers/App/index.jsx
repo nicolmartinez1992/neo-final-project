9c7d99c19797d26091dc2babdb419b86c692472e
valencha <nicolmartinez1992@hotmail.com>
2023-10-26 17:32:14 -0300

    Router setup

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from 'react-router-dom';
 
 import { ROUTES } from 'Data/constants';
 
-import Home from 'Containers/Home';
+// import Home from 'Containers/Home';
 
 import './index.scss';
+import Home from 'Containers/Home';
+import Login from 'Components/login';
 
-const BrowserRouter = createBrowserRouter([
-  {
-    element: <Home />,
-    path: ROUTES.home,
-  },
-]);
+// const BrowserRouter = createBrowserRouter([
+//   {
+//     element: <Home />,
+//     path: ROUTES.home,
+//   },
+// ]);
 
-const App = () => (<RouterProvider router={BrowserRouter} />);
+// const App = () => (<RouterProvider router={BrowserRouter} />);
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path={ROUTES.home} element={<Home />}>
+        <Route path={ROUTES.login} element={<Login />}></Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
