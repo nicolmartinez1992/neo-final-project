787621ce90ff06c3a1de6d78914d68bcaea37ba9
valencha <nicolmartinez1992@hotmail.com>
2023-11-04 12:07:28 -0300

    Error

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss';
+
+const Error = () => (
+  <div className="error">
+    <img
+      className="error__image"
+      src="https://img.freepik.com/vector-gratis/ups-error-404-ilustracion-concepto-robot-roto_114360-5529.jpg?w=1380&t=st=1699106772~exp=1699107372~hmac=bd9e62137f82a416eaabe2d3eb4c05db1f3d8bc30508a83e79d9fb84d2569e2f"
+      alt="error404"
+    />
+  </div>
+);
+
+export default Error;
--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,62 +1,88 @@
 import React, { useEffect } from 'react';
+import { Circles } from 'react-loader-spinner';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import getUser from '../../api/user';
 import './index.scss';
+import Error from 'Components/Error';
 
 const Profile = () => {
   const [userData, setUserData] = useState({});
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
 
   useEffect(() => {
     getUser(id)
       .then((response) => {
         setUserData(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.log('Error retrieving user data', error);
+        setLoading(false);
       });
   }, []);
 
+  if (!userData) {
+    return <Error />;
+  }
+
   return (
     <div className="profile">
-      <div className="profile__container">
-        <h1 className="profile__title">User Profile</h1>
-        <div className="profile__info">
-          <div className="profile__data1">
-            <span className="profile__data">
-              First Name:{' '}
-              {userData.name ? userData.name.firstname : ''}
-            </span>
-            <span className="profile__data">
-              Email: {userData.email}
-            </span>
-            <span className="profile__data">
-              Street:{' '}
-              {userData.address ? userData.address.street : ''}
-            </span>
-            <span className="profile__data">
-              City: {userData.address ? userData.address.city : ''}
-            </span>
-          </div>
-          <div className="profile__data2">
-            <span className="profile__data">
-              Last Name: {userData.name ? userData.name.lastname : ''}
-            </span>
-            <span className="profile__data">
-              Phone: {userData.phone}
-            </span>
-            <span className="profile__data">
-              Number:{' '}
-              {userData.address ? userData.address.number : ''}
-            </span>
-            <span className="profile__data">
-              Zip code:{' '}
-              {userData.address ? userData.address.zipcode : ''}
-            </span>
+      {loading ? (
+        <div className="profile__loading-container">
+          <span className="profile__loading-title">Loading...</span>
+          <Circles
+            className="profile__loading"
+            height="80"
+            width="80"
+            color="#000000"
+            ariaLabel="circles-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            marginTop="20px"
+          />
+        </div>
+      ) : (
+        <div className="profile__container">
+          <h1 className="profile__title">User Profile</h1>
+          <div className="profile__info">
+            <div className="profile__data1">
+              <span className="profile__data">
+                First Name:{' '}
+                {userData.name ? userData.name.firstname : ''}
+              </span>
+              <span className="profile__data">
+                Email: {userData.email}
+              </span>
+              <span className="profile__data">
+                Street:{' '}
+                {userData.address ? userData.address.street : ''}
+              </span>
+              <span className="profile__data">
+                City: {userData.address ? userData.address.city : ''}
+              </span>
+            </div>
+            <div className="profile__data2">
+              <span className="profile__data">
+                Last Name:{' '}
+                {userData.name ? userData.name.lastname : ''}
+              </span>
+              <span className="profile__data">
+                Phone: {userData.phone}
+              </span>
+              <span className="profile__data">
+                Number:{' '}
+                {userData.address ? userData.address.number : ''}
+              </span>
+              <span className="profile__data">
+                Zip code:{' '}
+                {userData.address ? userData.address.zipcode : ''}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
