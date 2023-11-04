2d58ecf1d5468b52a8404284a8a63d59f4e5c889
valencha <nicolmartinez1992@hotmail.com>
2023-11-04 10:37:16 -0300

    Handle cart error

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { Circles } from 'react-loader-spinner';
 import CartProduct from 'Components/CartProduct';
 import getCart from '../../api/cart';
 import getProducts from '../../api/products';
@@ -8,6 +9,7 @@ import './index.scss';
 const Cart = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
 
   useEffect(() => {
@@ -34,24 +36,57 @@ const Cart = () => {
             }
           });
         setCart(filteredProducts);
+        setLoading(false);
       } catch (error) {
         console.log('Error retrieving data', error);
+        setLoading(false);
       }
     };
 
     fetchData();
   }, [id]);
 
+  if (loading) {
+    return (
+      <div className="cart__loading-container">
+        <span className="cart__loading-title">Loading...</span>
+        <Circles
+          className="cart__loading"
+          height="80"
+          width="80"
+          color="#000000"
+          ariaLabel="circles-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          marginTop="20px"
+        />
+      </div>
+    );
+  }
+
   return (
     <div className="cart">
-      <div className="cart__container">
-        {cart.map((product) => (
-          <CartProduct key={product.id} {...product} />
-        ))}
-      </div>
-      <div className="cart__button-container">
-        <button className="cart__button">Buy Cart</button>
-      </div>
+      {cart.length === 0 ? (
+        <div className="cart__empty-warn-container">
+          <img
+            className="cart__empty-warn-image"
+            src="https://cdni.iconscout.com/illustration/premium/thumb/empty-cart-7359557-6024626.png"
+            alt=""
+          />
+          <Link to="/" className="cart__empty-warn-button">
+            CONTINUE SHOPPING
+          </Link>
+        </div>
+      ) : (
+        <div className="cart__container">
+          {cart.map((product) => (
+            <CartProduct key={product.id} {...product} />
+          ))}
+          <div className="cart__button-container">
+            <button className="cart__button">Buy Cart</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
