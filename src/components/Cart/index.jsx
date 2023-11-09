cbb1d154b4915e447438596614d52cf875cce037
valencha <nicolmartinez1992@hotmail.com>
2023-11-08 21:07:06 -0300

    Add cart error

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import CartProduct from 'Components/CartProduct';
 import Loading from 'Components/Loading';
+import Error from 'Components/Error';
 import getCart from '../../api/cart';
 import getProducts from '../../api/products';
 import './index.scss';
 import cartImage from '../../assets/images/cart.png';
+import errorImage from '../../assets/images/error.png';
 
 const Cart = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
   const { id } = useParams();
 
   useEffect(() => {
@@ -22,6 +25,11 @@ const Cart = () => {
         const cartResponse = await getCart(id);
         const productsResponseData = productsResponse.data;
         const cartResponseData = cartResponse.data;
+
+        if (cartResponseData === null) {
+          setErrorMessage('cart id should be provided');
+        }
+
         const filteredProducts =
           cartResponseData.products &&
           cartResponseData.products.map((prod) => {
@@ -39,13 +47,21 @@ const Cart = () => {
         setCart(filteredProducts);
         setLoading(false);
       } catch (error) {
-        console.log('Error retrieving data', error);
         setLoading(false);
+        setErrorMessage(error?.response?.data?.message);
       }
     };
 
     fetchData();
   }, [id]);
+
+  if (errorMessage === 'cart id should be provided') {
+    return (
+      <div className="cart__error">
+        <Error title="THIS ID DOES NOT EXIST" image={errorImage} />
+      </div>
+    );
+  }
 
   if (loading) {
     return <Loading />;
@@ -55,6 +71,9 @@ const Cart = () => {
     <div className="cart">
       {cart.length === 0 ? (
         <div className="cart__warning-container">
+          <h1 className="cart__warning-title">
+            NO ITEMS IN THIS CART
+          </h1>
           <img
             className="cart__warning-image"
             src={cartImage}
